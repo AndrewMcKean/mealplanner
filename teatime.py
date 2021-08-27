9194a6b31bb5a73ac4781af36946694f8a0f7bc7
Andrew McKean <a.mckean90@outlook.com>
2021-08-27 23:33:32 +0100

    Fixed broken variables after they had been moved to data file

--- teatime.py
+++ teatime.py
@@ -1,30 +1,31 @@
 #Teatime
 import random
-import mealsData
+import mealsData as data
 
 #Input: Three string arguments, these must be capitalised days of the week
 #Output: Mealplan for dinners for the week. Input days are not assigned dinner.
 def daysThree(day1, day2, day3):
     working = [day1, day2, day3]
-    threeDayWeek = random.sample(dinners, 4)
+    threeDayWeek = random.sample(data.dinners, 4)
     index = 0
-    for x in mealsDict.keys():
+    for x in data.mealsDict.keys():
         if x not in working:
-            mealsDict.update({x : threeDayWeek[index]})
+            data.mealsDict.update({x : threeDayWeek[index]})
             index += 1
-    for i in mealsDict.items():
+    for i in data.mealsDict.items():
         print(i)
 
 #Input: Four string arguments, these must be capitalised days of the week
 #Output: Mealplan for dinners for the week. Input days are not assigned dinner.
 def daysFour(day1, day2, day3, day4):
     working = [day1, day2, day3, day4]
-    fourDayWeek = random.sample(dinners, 3)
+    fourDayWeek = random.sample(data.dinners, 3)
     index = 0
-    for x in mealsDict.keys():
+    for x in data.mealsDict.keys():
         if x not in working:
-            mealsDict.update({x : fourDayWeek[index]})
+            data.mealsDict.update({x : fourDayWeek[index]})
             index += 1
-    for i in mealsDict.items():
+    for i in data.mealsDict.items():
         print(i)
                 
+daysThree('Monday', 'Tuesday', 'Wednesday')
